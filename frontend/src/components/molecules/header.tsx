b152b5660571fbcfae03df9d93501b93b428111b
Yasmin S. Barata <162582195+Yas-moon@users.noreply.github.com>
2025-07-13 23:43:53 -0300

    :bug: fix: ajuste header

--- frontend/src/components/molecules/header.tsx
+++ frontend/src/components/molecules/header.tsx
@@ -8,24 +8,24 @@ export default function Header() {
   const router = useRouter();
   return (
     <div className="shadow-lg fixed top-0 z-30 flex h-21.25 w-lvw items-center justify-center bg-redSecundary ">
-      <div className="flex h-full w-full max-w-[1440px] items-center justify-end gap-3 px-7.5 lg:px-10 xl:px-20">
+      <div className="flex h-full w-full max-w-[1440px] items-center justify-center gap-3 px-7.5 lg:px-10 xl:px-20">
         <div className="hidden w-full max-w-[612px] items-center justify-between gap-2 lg:flex">
           <Button
-            className="text-black font-semibold"
+            className="text-whiteBG font-semibold"
             variant="secondary"
             onClick={() => router.push("/")}
           >
             Início
           </Button>
           <Button
-            className="text-black font-semibold"
+            className="text-whiteBG font-semibold"
             variant="secondary"
             onClick={() => router.push("/livros")}
           >
             Lista de livros
           </Button>
           <Button
-            className="text-black font-semibold"
+            className="text-whiteBG font-semibold"
             variant="secondary"
             onClick={() => router.push("/livros/novo")}
           >
